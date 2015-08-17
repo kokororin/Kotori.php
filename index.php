4923f3647cf939078459a730265ebc482bd6bed1
Kokororin <ritsuka.sunny@gmail.com>
2015-08-17 16:35:31 +0800

    添加QUERY_STRING的支持

--- index.php
+++ index.php
@@ -2,12 +2,13 @@
 require './Kotori.class.php';
 //载入配置
 $config = array(
-    'APP_PATH'    => './App/', //项目目录
-    'DB_HOST'     => '127.0.0.1',
-    'DB_PORT'     => '3306',
-    'DB_USER'     => 'root',
-    'DB_PWD'      => 'root',
-    'DB_NAME'     => 'typecho',
+    'APP_PATH' => './App/', //项目目录
+    'DB_HOST' => '127.0.0.1',
+    'DB_PORT' => '3306',
+    'DB_USER' => 'root',
+    'DB_PWD' => 'root',
+    'DB_NAME' => 'typecho',
     'USE_SESSION' => true,
+    'URL_MODE' => 'QUERY_STRING',
 );
 Kotori::getInstance($config)->run();
