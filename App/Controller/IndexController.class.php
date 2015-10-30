811b1c7868a2c4909598be0f5d84635b082b43ae
Kokororin <ritsuka.sunny@gmail.com>
2015-10-30 17:15:51 +0800

    删除项目目录中的文档

--- App/Controller/IndexController.class.php
+++ App/Controller/IndexController.class.php
@@ -8,9 +8,6 @@ class IndexController extends Controller
 
     public function index()
     {
-        $data = file_get_contents(PUBLIC_DIR . '/HELP.md');
-        $content = Markdown::convert($data);
-        $this->assign('content', $content);
         $this->display();
     }
 
