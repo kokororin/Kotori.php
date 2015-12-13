73358c7441e8ba4b63d5ee4b84a035e9faf74dc8
空樱酱 <ritsuka.sunny@gmail.com>
2015-12-13 12:28:26 +0800

    兼容旧版本

--- index.php
+++ index.php
@@ -2,12 +2,12 @@
 require './Kotori.class.php';
 
 Kotori::run(array(
-    'APP_PATH'  => './App/',
-    'DB_HOST'   => '127.0.0.1',
-    'DB_USER'   => 'root',
-    'DB_PWD'    => 'root',
-    'DB_NAME'   => 'test',
-    'URL_ROUTE' => array(
-        'news/([0-9])' => 'Index/showNews/$1',
-    ),
+	'APP_PATH'  => './App/',
+	'DB_HOST'   => '127.0.0.1',
+	'DB_USER'   => 'root',
+	'DB_PWD'    => 'root',
+	'DB_NAME'   => 'test',
+	'URL_ROUTE' => array(
+		'news/([0-9])' => 'Index/showNews/$1',
+	),
 ));
