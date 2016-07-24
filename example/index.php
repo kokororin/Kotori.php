4942f5e083b6f43fc6baeb63bef15edfd9b5435e
Kokororin <i@kotori.love>
2016-07-24 11:40:16 +0800

    路由分组

--- example/index.php
+++ example/index.php
@@ -1,4 +1,12 @@
 <?php
+
+/**
+ * Require the Kotori.php using Composer's autoloader
+ *
+ * If you are not using Composer, you need to load Kotori.php with your own
+ * PSR-4 autoloader.
+ */
+
 require __DIR__ . '/../vendor/autoload.php';
 
 $config = [
@@ -11,13 +19,24 @@ $config = [
     'DB_USER' => 'root',
     'DB_PWD' => '123456',
     'DB_NAME' => 'test',
+    // 'URL_ROUTE' => [
+    //     '/' => 'Hello/index',
+    //     'news/([0-9])' => 'Hello/showNews/$1',
+    //     'add' => [
+    //         'get' => 'Hello/addNews',
+    //         'post' => 'Hello/insertNews',
+    //         'delete' => 'Hello/deleteNews',
+    //     ],
+    // ],
     'URL_ROUTE' => [
-        '/' => 'Hello/index',
-        'news/([0-9])' => 'Hello/showNews/$1',
-        'add' => [
-            'get' => 'Hello/addNews',
-            'post' => 'Hello/insertNews',
-            'delete' => 'Hello/deleteNews',
+        '1.kotori.php' => [
+            '/' => 'Hello/index',
+            'news/([0-9])' => 'Hello/showNews/$1',
+            'add' => [
+                'get' => 'Hello/addNews',
+                'post' => 'Hello/insertNews',
+                'delete' => 'Hello/deleteNews',
+            ],
         ],
     ],
 ];
