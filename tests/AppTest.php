71c198b552aa908e4b4e0ba30089e377c47419d1
Kokororin <i@kotori.love>
2017-02-12 12:18:57 +0800

    添加Mysql测试

--- tests/AppTest.php
+++ tests/AppTest.php
@@ -3,9 +3,22 @@ namespace Kotori\Tests;
 
 use Kotori\App;
 use Kotori\Core\Config;
+use Kotori\Core\Database;
+use PDO;
+use PDOException;
+use PHPUnit_Framework_TestCase;
 
-class AppTest extends \PHPUnit_Framework_TestCase
+class AppTest extends PHPUnit_Framework_TestCase
 {
+    const MYSQL_HOST = '127.0.0.1';
+    const MYSQL_USER = 'root';
+    const MYSQL_PWD = '';
+    const MYSQL_DB = 'kotori_php_test_db';
+
+    public function __construct()
+    {
+        $this->createTestDatabase();
+    }
 
     public function testApp()
     {
@@ -34,5 +47,83 @@ class AppTest extends \PHPUnit_Framework_TestCase
         $config = new Config();
         $config->initialize();
         $this->assertTrue(is_array($config->getArray()));
+    }
+
+    protected function createTestDatabase()
+    {
+        try {
+            $pdo = new PDO('mysql:host=' . self::MYSQL_HOST, self::MYSQL_USER, self::MYSQL_PWD);
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $pdo->exec('DROP DATABASE IF EXISTS `' . self::MYSQL_DB . '`;
+CREATE DATABASE `' . self::MYSQL_DB . '`;
+USE `' . self::MYSQL_DB . '`;
+
+DROP TABLE IF EXISTS `table`;
+CREATE TABLE `table` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;');
+        } catch (PDOException $e) {
+            throw $e;
+        }
+
+    }
+
+    protected function getDatabaseInstance()
+    {
+        $config = Config::getSoul();
+        $config->initialize([
+            'APP_DEBUG' => false,
+            'DB' => [
+                'db' => [
+                    'TYPE' => 'mysql',
+                    'NAME' => self::MYSQL_DB,
+                    'HOST' => self::MYSQL_HOST,
+                    'USER' => self::MYSQL_USER,
+                    'PWD' => self::MYSQL_PWD,
+                ],
+            ],
+        ]);
+        $database = Database::getSoul();
+        return $database;
+    }
+
+    public function testInsert()
+    {
+        $insertId = $this->getDatabaseInstance()
+            ->insert('table', [
+                'id' => 1,
+                'name' => 'kotori',
+            ]);
+        $this->assertNotEquals(0, $insertId);
+    }
+
+    public function testSelect()
+    {
+        $datas = $this->getDatabaseInstance()
+            ->select('table', '*', [
+                'id' => 1,
+            ]);
+        $this->assertEquals('kotori', $datas[0]['name']);
+    }
+
+    public function testUpdate()
+    {
+        $effectedRows = $this->getDatabaseInstance()
+            ->update('table', [
+                'name' => 'honoka',
+            ], [
+                'id' => 1,
+            ]);
+        $this->assertGreaterThan(0, $effectedRows);
+    }
+
+    public function testDelete()
+    {
+        $effectedRows = $this->getDatabaseInstance()
+            ->delete('table', [
+                'id' => 1,
+            ]);
+        $this->assertGreaterThan(0, $effectedRows);
     }
 }
